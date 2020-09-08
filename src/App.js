7dba5331d9764c0bad7b3f61478ebe9af3c1821f
Jay <thk116@gmail.com>
2020-09-08 00:27:31 -0700

    app is functional

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import News from './News';
 function App() {
   return (
     <div className="App">
-      <div className="container main-container">
+      <div className="container-fluid main-container">
         <div className="row justify-content-center">
           <div className="jumbotron header">
             <h1 className="display-4 title">purple news</h1>
             <hr className="my-4 title-divider"/>
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p>reading between the party lines</p>
           </div>
         </div>
         <div className="row justify-content-around">
             <News source={'CNN'} color='red'></News>
-            <News source={'Fox'} color='blue'></News>
+            <News source={'Fox'} color='dodgerblue'></News>
         </div>
       </div>
     </div>
--- src/News.js
+++ src/News.js
@@ -3,17 +3,43 @@ import './App.css';
 
 function News(props) {
 
+    const [news, setNews] = useState([])
+
+    let color = props.color === 'red' ? 'danger' : 'primary'
+    let newsData = [...news]
+    let newsItems = newsData.map((item, i) => {
+            return (
+                <div className='articles'>
+                    <a href={item.url} className='links' key={i}>{item.title}</a>
+                    <hr style={{backgroundColor: `${props.color}`}}/>
+                </div>
+            )
+    })
+    let scraping = (
+        <div>
+            <div className={`spinner-border text-${color}`} role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+            <h3 className='loading'>scraping articles...</h3>
+        </div>
+    )
+
     useEffect(() => {
         console.log(`gotcha ${props.source}!`)
-        fetch(`http://localhost:3000/${props.source}`)
+        fetch(`http://localhost:1337/${props.source}`)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setNews(data))
             .catch(err => console.log('err: ', err))
-    });
+    }, [props.source]);
 
     return (
-        <div className="col-5 news-block" style={{backgroundColor: `${props.color}`}}>
-            <h1 className='news-title'>{props.source}</h1>
+        <div className="col-5 news-block">
+            <h1 className='news-title' style={{color: `${props.color}`}}>{props.source}</h1>
+            {console.log('Got news: ', news, ' newsData: ', newsData)}
+            {newsData.length ? newsItems
+            :
+            scraping
+            }
         </div>
     );
 }
