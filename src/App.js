220b8c43aecfbb20908f90afb082976a3358743e
Jay <thk116@gmail.com>
2020-09-08 18:46:13 -0700

    added date and time

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import moment from 'moment';
 import News from './News';
 
 function App() {
@@ -7,10 +8,11 @@ function App() {
     <div className="App">
       <div className="container-fluid main-container">
         <div className="row justify-content-center">
-          <div className="jumbotron header">
+          <div className="jumbotron header justify-content-center">
             <h1 className="display-4 title">purple news</h1>
-            <hr className="my-4 title-divider"/>
             <p>reading between the party lines</p>
+            <hr className="my-4 title-divider"/>
+            <div className='date'>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
           </div>
         </div>
         <div className="row justify-content-around">
