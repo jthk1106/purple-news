a25a8b7d2c58eaf4fb64f79450c4b2d7a621be0c
Jay <thk116@gmail.com>
2020-09-13 02:12:09 -0700

    using axios and ran build

--- src/News.js
+++ src/News.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function News(props) {
 
@@ -26,9 +27,10 @@ function News(props) {
 
     useEffect(() => {
         console.log(`gotcha ${props.source}!`)
-        fetch(`https://purple-news-proxy.herokuapp.com/${props.source}`)
+        axios.get(`https://purple-news-proxy.herokuapp.com/${props.source}`)
+        // fetch(`https://purple-news-proxy.herokuapp.com/${props.source}`)
         // fetch(`http://localhost:1111/${props.source}`)
-            .then(res => res.json())
+            // .then(res => res.json())
             .then(data => setNews(data))
             .catch(err => console.log('err: ', err))
     }, [props.source]);
